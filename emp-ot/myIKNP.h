0582aaf766b9d5ff637cf10b399284679cb415f0
szn-nzs <sznnzs@pku.edu.cn>
2024-12-16 15:38:22 +0800

    remove comments

--- emp-ot/myIKNP.h
+++ emp-ot/myIKNP.h
@@ -47,14 +47,11 @@ public:
 			k0[i] = data[i];
 			G0[i].reseed(&k0[i]);
 		}
-		// printf("sss: %d\n", s[1]);
-		// print128_num(k0[1]);
 	}
 	
 	void sendPre(const vector<vector<block>>& U, int64_t length) {
 		q.clear();
 		q.resize(length);
-		// printf("1111111111111111111111111\n");
 
 		int64_t j = 0;
 		block local_out[block_size];
@@ -79,24 +76,16 @@ private:
 	void sendPreBlock(const vector<block>& U, block * out, int64_t len) {
 		block t[block_size];
 		memset(t, 0, block_size * sizeof(block));
-		// block tmp[block_size];
 		int64_t local_block_size = (len+127)/128*128;
-		// io->recv_block(tmp, local_block_size);
+		
 		for(int64_t i = 0; i < 128; ++i) {
 			// G
 			G0[i].random_data(t+(i*block_size/128), local_block_size/8);
 			if (s[i]) {
 				xorBlocks_arr(t+(i*block_size/128), t+(i*block_size/128), U.data()+(i*block_size/128), local_block_size/128);
-				// print128_num(t[i*block_size/128]);
 			}
-			// if (dbg && i == 100) {
-				// printf("send: %d\n", s[i]);
-				// print128_num(t[i*block_size/128]);
-			// }
 		}
 		sse_trans((uint8_t *)(out), (uint8_t*)t, 128, block_size);
-		// printf("send trans: ***********\n");
-		// print128_num(out[0]);
 	}
 };
 
@@ -115,8 +104,6 @@ public:
 			G0[i].reseed(&k0[i]);
 			G1[i].reseed(&k1[i]);
 		}
-		// print128_num(k0[1]);
-		// print128_num(k1[1]);
 	}
 	const Point& baseOTMsg1() {
 		return ot_sender.sendA();
@@ -149,7 +136,6 @@ public:
 		for (; j < length/block_size; ++j) {
 				vector<block> tmp = recvPreBlock(T.data()+j*block_size, std::span(block_r + (j*block_size/128), block_size));
 				res.emplace_back(std::move(tmp));
-			// res.emplace_back(std::move(tmp));
 		}
 		int64_t remain = length % block_size;
 		if (remain > 0) {
@@ -171,47 +157,22 @@ private:
 
 	vector<block> recvPreBlock(block *out, std::span<const block> r) {
 		int64_t len = r.size();
-		// t就是iknp里recv的矩阵T
-		// 是一个len*128矩阵
-		// 根据上面分段操作可知这里最多就是block_size*128矩阵
 		block t[block_size];
 		memset(t, 0, block_size*sizeof(block));
 		vector<block> tmp;
 		tmp.clear();
 		tmp.resize(block_size);
 
-		// 上取整到128的整数倍
 		int64_t local_block_size = (len+127)/128 * 128;
-		// 按列生成矩阵T，一共有128列
 		for(int64_t i = 0; i < 128; ++i) {
-			// t+(i*block_size/128)：每一列最大有block_size个比特，最多需要block_size/128个block，
-			// 所以第i列是从i*block_size/128位置开始放
-			// local_block_size/8：生成的随机byte数，是矩阵T实际的列向量长度
 			G0[i].random_data(t+(i*block_size/128), local_block_size/8);
 			G1[i].random_data(tmp.data()+(i*block_size/128), local_block_size/8);
-			// if (dbg && i == 100) {
-			// 	printf("recv1 g0&g1: \n");
-			// 	print128_num(t[i*block_size/128]);
-			// 	print128_num(tmp[i*block_size/128]);
-			// }
-			// 计算tmp=G(k0)^G(k1)^r
 			xorBlocks_arr(tmp.data()+(i*block_size/128), t+(i*block_size/128), tmp.data()+(i*block_size/128), local_block_size/128);
 			xorBlocks_arr(tmp.data()+(i*block_size/128), r.data(), tmp.data()+(i*block_size/128), local_block_size/128);
-			// if (dbg && i == 100) {
-			// 	printf("recv2 u&r:\n");
-			// 	print128_num(tmp[i*block_size/128]);
-			// 	print128_num(r[0]);
-			// }
 		}
 
-		// printf("trans*******: \n");
-		// for (int64_t i = 0; i < 30; ++i) {
-		// 	print128_num(t[i*block_size/128]);
-		// }
 		//转置 
 		sse_trans((uint8_t *)(out), (uint8_t*)t, 128, block_size);
-		// print128_num(out[0]);
-		// print128_num(r[0]);
 		return tmp;
 	}
 };
